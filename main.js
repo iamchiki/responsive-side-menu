aae56bff650f27966ef5ee5dc7a1d1b3be89a1e3
nikhil <nikhil.soni599@gmail.com>
2021-03-06 23:29:05 +0530

    closing js functionality

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function openSideBar() {
   document.getElementById('main').style.marginLeft = '250px';
   document.getElementById('side-menu').style.width = '250px';
 }
+
+function closeSideBar() {
+  document.getElementById('main').style.marginLeft = '0px';
+  document.getElementById('side-menu').style.width = '0px';
+}
